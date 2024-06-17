761f388fae5fbb6be81d9ab847c24d40ff966d88
xXTheSupremeXx <121276193+xXTheSupremeXx@users.noreply.github.com>
2024-06-17 09:09:50 -0500

    Update vending_machine.py

--- MSA_Main_Code/vending_machine.py
+++ MSA_Main_Code/vending_machine.py
@@ -1,17 +1,18 @@
 def coin():
-    while True:
-        starting_amount = 50
+    starting_amount = 50
+    valid_coins = [1, 5, 10, 25]
+    print(f'Amount Due: {starting_amount}')
+    while starting_amount > 0:
         try:
-            insert_coin = int(input(f'Amount Due:'))
-            if (insert_coin < 0):
-                continue
-            elif insert_coin not in [1, 5, 10, 25]:
-                continue
+            insert_coin = int(input())
+            if insert_coin in valid_coins:
+                starting_amount -= insert_coin
+                print(f'Amount: {starting_amount}')
+            if starting_amount <= 0:
+                starting_amount *= -1
+                break
             else:
-                return insert_coin
+                continue
         except:
             break
-    
-
-
-
+coin()
